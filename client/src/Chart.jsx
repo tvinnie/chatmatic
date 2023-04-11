37ca1a80d2ed5df119cbef1bfa3c5441fe45c230
tvinnie <tvinnie77@gmail.com>
2023-04-11 16:52:37 +0300

    Function client and server module for graphql with apollo client

--- client/src/Chart.jsx
+++ client/src/Chart.jsx
@@ -3,21 +3,31 @@ import {
     ApolloClient,
     InMemoryCache, 
     ApolloProvider,
-    useQuery, 
+    useSubscription, 
     useMutation,
     gql 
 } from '@apollo/client';
 
+import { WebSocketLink } from '@apollo/client/link/ws';
 import { Container, Row, Col, FormInput, Button  } from "shards-react";
 
+
+const link = new WebSocketLink({
+    uri: "ws://localhost:4000/",
+    options: {
+        reconnect: true
+    }
+})
+
 const client = new ApolloClient({
+    link,
     //point to the graphql server
     uri: "http://localhost:4000/",
     cache: new InMemoryCache()
 });
 
 const GET_MESSAGES = gql `
-query {
+subscription {
     messages {
       id,
       content,
@@ -33,9 +43,7 @@ mutation ($user: String!, $content: String!) {
 `;
 
 const Messages = ({ user }) => {
-    const { data } = useQuery(GET_MESSAGES, {
-        pollInterval: 500,
-    });
+    const { data } = useSubscription(GET_MESSAGES);
      if (!data) {
         return null;
      }
